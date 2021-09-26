0c62dd40d05dc48fccc6f1a7b1f6bf81acce0f64
SuperSo6 <89916346+SuperSo6@users.noreply.github.com>
2021-09-27 01:11:12 +0200

    Add files via upload

--- DorkX.py
+++ DorkX.py
@@ -8,7 +8,7 @@ cur = con.cursor()
  #   print(i)
 
 def insertion(input_name,description): #Pour inserer dans Operator_Description 
-    cur.execute(f"INSERT INTO Operator_Description VALUES ('{input_name}','{description}','{none}')")
+    cur.execute(f"INSERT INTO Operator_Description VALUES ('{input_name}','{description}','{None}')")
     con.commit()
     for i in cur.execute('SELECT * FROM Operator_Description'):
         print(i)
@@ -21,7 +21,21 @@ print(name,desc)
 
 insertion(name,desc) 
 '''
+def afficher_description(operator_name=None):
+    tab=[]
+    desc =[]
+    for i in cur.execute(f"SELECT Name,Description FROM Operator_Description ;"):
+        tab.append(i)
+    print("[Name: ","    ", " Description]")
+    print(" ")
+    print(" ")
+    for j in tab:
+        print (j[0],' ',j[1],"\n")
+        
+    
 
+#operator_name=input("operator ?")
+afficher_description()
 con.close()
 
 
